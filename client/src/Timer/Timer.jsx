aed72da5663bb82ecf6139350d8586e2f8404d87
edgesean <seandillon59@gmail.com>
2021-06-21 21:14:53 +0200

    finished concept

--- client/src/Timer/Timer.jsx
+++ client/src/Timer/Timer.jsx
@@ -17,15 +17,20 @@ export const Timer = () => {
     if(isMinutes)setTime(e.target.value);
     else setTime(e.target.value*60);
   } 
-  const buttonUp = () => setTime(timeLeft+1);
-  const buttonDown = () => {if (timeLeft > 0) setTime(timeLeft-1)};
+  const buttonUp = () => {
+    if (timeLeft < 600) {
+      if (isMinutes) setTime(timeLeft+1)
+      if (!isMinutes) setTime(timeLeft+60)
+    }
+  };
+  const buttonDown = () => {if (timeLeft > 0) setTime(isMinutes ? timeLeft-1 : timeLeft-60)};
   const minsSwitch = () => isMinutes ? setIsMinutes(false) : setIsMinutes(true);
   const powerBtn = () => power ? setPower(false) : setPower(true);
   
 
   useEffect(() => {
     if (timeLeft > 0 && power) {
-      const timerInterval = setInterval(() => setTime(timeLeft-1), 1000)
+      const timerInterval = setInterval(() => setTime(timeLeft-1), 60000)
       return () => clearInterval(timerInterval);
     }
     
@@ -45,7 +50,8 @@ export const Timer = () => {
             </div>
             { power ?
             <div className="font-face-digi time">
-              {isMinutes ? `${timeLeft} MIN` : `${Math.floor(timeLeft/60)} HR`}
+              {isMinutes ? `${timeLeft} ` : `${Math.floor(timeLeft/60)}`}
+              <div className="digi-resize">{isMinutes ? 'MIN' : 'HRS'} </div>
             </div> : <div></div>
             }
           </div>
@@ -55,14 +61,24 @@ export const Timer = () => {
               <div className="bttn" id="up" onClick={buttonUp}><img src={upSvg} alt="up" /></div>
               <div className="bttn" id="down" onClick={buttonDown}><img src={downSvg} alt="down" /></div>
             </div>
-            <div className="bttn power" onClick={powerBtn}>{power ? <div></div> : null}PWR</div>
+            <div className="bttn power" onClick={powerBtn}>{power ? <div className="circ"></div> : <div className="no-circ"></div> }PWR</div>
           </div>
 
         </div>
 
         <div className="second-row">
-          <div className="bttn" onClick={minsSwitch}>hrs/mins</div>
-          <input type="range" min={0} max={100} value={timeLeft} onChange={sliderChange} />
+          <div className="hrs-bttn-container">
+            Mins
+                      <div className="mins-toggle" onClick={minsSwitch}> {isMinutes ? <div className="left"></div> : <div className="right"></div>} </div>
+            Hrs
+          </div>
+          <div className="slider-container">
+            {
+              isMinutes ?
+            <input className="hours-slider" type="range" min={0} max={600} value={timeLeft} onChange={sliderChange} /> :
+            <input className="minutes-slider" type="range" min={0} max={10} value={timeLeft/60} onChange={sliderChange} />
+            }
+          </div>
         </div>
       </div>
       
